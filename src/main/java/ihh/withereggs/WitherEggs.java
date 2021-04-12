b5448c00d4e64e72394dab9f00034e1676dc1718
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-04-12 16:45:59 +0200

    Initial commit

---
+++ src/main/java/ihh/withereggs/WitherEggs.java
@@ -0,0 +1,26 @@
+package ihh.withereggs;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod("withereggs")
+public class WitherEggs {
+    public WitherEggs() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "withereggs");
+    static {
+        ITEMS.register("wither_spawn_egg", () -> new SpawnEggItem(EntityType.WITHER, 0x1f1f1f, 0x6b8a8a, new Item.Properties().group(ItemGroup.MISC)));
+        ITEMS.register("ender_dragon_spawn_egg", () -> new SpawnEggItem(EntityType.ENDER_DRAGON, 0x0e0e0e, 0xcc00fa, new Item.Properties().group(ItemGroup.MISC)));
+        ITEMS.register("iron_golem_spawn_egg", () -> new SpawnEggItem(EntityType.IRON_GOLEM, 0xe2dbd6, 0x74a332, new Item.Properties().group(ItemGroup.MISC)));
+        ITEMS.register("snow_golem_spawn_egg", () -> new SpawnEggItem(EntityType.SNOW_GOLEM, 0xffffff, 0xe38a1d, new Item.Properties().group(ItemGroup.MISC)));
+        ITEMS.register("illusioner_spawn_egg", () -> new SpawnEggItem(EntityType.ILLUSIONER, 0x959b9b, 0x135993, new Item.Properties().group(ItemGroup.MISC)));
+        ITEMS.register("giant_spawn_egg", () -> new SpawnEggItem(EntityType.GIANT, 0x799c65, 0x00afaf, new Item.Properties().group(ItemGroup.MISC)));
+    }
+}
