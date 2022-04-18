328d833b9ccebff7dfb8597eb48273f71dcd7ce3
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-04-18 13:37:10 +0200

    1.18.2

--- src/main/java/ihh/withereggs/WitherEggs.java
+++ src/main/java/ihh/withereggs/WitherEggs.java
@@ -12,15 +12,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 @Mod("withereggs")
 public class WitherEggs {
     public WitherEggs() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "withereggs");
-    static {
+        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "withereggs");
         ITEMS.register("wither_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.WITHER, 0x1f1f1f, 0x6b8a8a, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("ender_dragon_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.ENDER_DRAGON, 0x0e0e0e, 0xcc00fa, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("iron_golem_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.IRON_GOLEM, 0xe2dbd6, 0x74a332, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("snow_golem_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.SNOW_GOLEM, 0xffffff, 0xe38a1d, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("illusioner_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.ILLUSIONER, 0x959b9b, 0x135993, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("giant_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.GIANT, 0x799c65, 0x00afaf, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
