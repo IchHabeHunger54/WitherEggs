ef9a28b1b51b4481a8022714b641375ce0bb4e7f
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-05-28 00:42:09 +0200

    1.19.2 update, forgot to push lololol

--- src/main/java/ihh/withereggs/WitherEggs.java
+++ src/main/java/ihh/withereggs/WitherEggs.java
@@ -9,10 +9,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod("withereggs")
+@Mod(WitherEggs.MOD_ID)
 public class WitherEggs {
+    public static final String MOD_ID = "withereggs";
+
     public WitherEggs() {
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "withereggs");
+        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WitherEggs.MOD_ID);
         ITEMS.register("wither_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.WITHER, 0x1f1f1f, 0x6b8a8a, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("ender_dragon_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.ENDER_DRAGON, 0x0e0e0e, 0xcc00fa, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
         ITEMS.register("iron_golem_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.IRON_GOLEM, 0xe2dbd6, 0x74a332, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
