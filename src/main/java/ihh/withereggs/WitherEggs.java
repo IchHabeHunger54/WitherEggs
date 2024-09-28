9bcc1206de9153e02c1d3b1cffd662a80b20a15e
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-09-28 18:18:23 +0200

    1.21.1 update

--- src/main/java/ihh/withereggs/WitherEggs.java
+++ src/main/java/ihh/withereggs/WitherEggs.java
@@ -3,25 +3,22 @@ package ihh.withereggs;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.common.ForgeSpawnEggItem;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(WitherEggs.MOD_ID)
 public class WitherEggs {
     public static final String MOD_ID = "withereggs";
 
-    public WitherEggs() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WitherEggs.MOD_ID);
+    public WitherEggs(IEventBus bus) {
+        DeferredRegister.Items ITEMS = DeferredRegister.createItems(WitherEggs.MOD_ID);
         Item.Properties properties = new Item.Properties();
-        RegistryObject<ForgeSpawnEggItem> illusionerSpawnEgg = ITEMS.register("illusioner_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.ILLUSIONER, 0x959b9b, 0x135993, properties));
-        RegistryObject<ForgeSpawnEggItem> giantSpawnEgg = ITEMS.register("giant_spawn_egg", () -> new ForgeSpawnEggItem(() -> EntityType.GIANT, 0x799c65, 0x00afaf, properties));
+        DeferredItem<DeferredSpawnEggItem> illusionerSpawnEgg = ITEMS.register("illusioner_spawn_egg", () -> new DeferredSpawnEggItem(() -> EntityType.ILLUSIONER, 0x959b9b, 0x135993, properties));
+        DeferredItem<DeferredSpawnEggItem> giantSpawnEgg = ITEMS.register("giant_spawn_egg", () -> new DeferredSpawnEggItem(() -> EntityType.GIANT, 0x799c65, 0x00afaf, properties));
         ITEMS.register(bus);
         bus.<BuildCreativeModeTabContentsEvent>addListener(e -> {
             if (e.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
